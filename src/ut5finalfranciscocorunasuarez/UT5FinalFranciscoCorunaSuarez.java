2ba05a8f6f6a5fe7b31ed2fe24153f331e17c114
Lotoamarillo <frandiossupremo@gmail.com>
2016-06-17 15:16:48 +0100

    paso 1 y 2

---
+++ src/ut5finalfranciscocorunasuarez/UT5FinalFranciscoCorunaSuarez.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5finalfranciscocorunasuarez;
+
+/**
+ *
+ * @author fran
+ */
+public class UT5FinalFranciscoCorunaSuarez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public class Gasolinera {
+    private int litrosVendidosMesActual98;
+    private int litrosVendidosMesActual95;
+    private int litrosVendidosMesActualDiesel;
+    private String direccion;
+    }
+    
+}
